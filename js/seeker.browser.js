76cc97f6b1eac4b739f5bd56f27459e8733ee537
Damian Kao <Damian@Damians-MacBook-Pro.local>
2013-06-26 10:32:01 +0100

    started browser component

---
+++ js/seeker.browser.js
@@ -0,0 +1,18 @@
+(function() {
+	/*
+	Author: Damian Kao
+	E-mail: damian.kao@gmail.com
+
+	data = {
+		
+	}
+
+	*/
+	seeker.browser = function() {
+		var base = new seeker.base('div');
+
+
+
+		return base;
+	}
+}());
